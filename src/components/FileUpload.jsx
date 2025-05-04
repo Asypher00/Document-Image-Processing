a7246d3043e0a805fdbcb69f59d736e54d1c87e7
Neeraj Agrawal <shashineerajagrawal@gamil.com>
2025-05-05 04:18:15 +0530

    Fix image paths for Netlify deployment

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,11 +1,12 @@
 import {Buffer} from "buffer";
+import documentIcon from "../../public/document-icon.png"
 const FileUpload = ({setFileUploaded}) => {
     const handleFileUpload = async (event) =>{
         const fileUpload = await event.target.files[0].arrayBuffer();
         const file = {
             type: event.target.files[0].type,
             file: Buffer.from(fileUpload).toString("base64"),
-            imageURL: event.target.files[0].type.includes("pdf") ? "../../public/document-icon.png" : URL.createObjectURL(event.target.files[0])
+            imageURL: event.target.files[0].type.includes("pdf") ? documentIcon : URL.createObjectURL(event.target.files[0])
         }
         setFileUploaded(file);
     } 
